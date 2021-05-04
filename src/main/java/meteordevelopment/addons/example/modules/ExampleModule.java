e875c8cfa94ed35bc519de344f4ed9bd747befe7
SpwCode <83662999+SpwCode@users.noreply.github.com>
2021-05-04 22:11:44 +0500

    Update ExampleModule.java

--- src/main/java/meteordevelopment/addons/example/modules/ExampleModule.java
+++ src/main/java/meteordevelopment/addons/example/modules/ExampleModule.java
@@ -2,6 +2,32 @@ package meteordevelopment.addons.example.modules;
 
 import meteordevelopment.addons.example.ExampleAddon;
 import minegame159.meteorclient.systems.modules.Module;
+import com.google.common.collect.Streams;
+import meteordevelopment.orbit.EventHandler;
+import minegame159.meteorclient.events.world.TickEvent;
+import minegame159.meteorclient.systems.modules.Categories;
+import minegame159.meteorclient.systems.modules.Modules;
+import minegame159.meteorclient.systems.modules.player.AutoEat;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+ @EventHandler
+    private void onTick(TickEvent.Post event) {
+        if (mc.player == null) return;
+        if (mc.world == null) return;
+        if (mc.interactionManager == null) return;
+
+        if (mc.player.getPosY() != 4) {
+            
+        }
+    }
 
 public class ExampleModule extends Module {
     public ExampleModule() {
