37ea6358672c3809d8aa500946b82bc65b99f904
SpwCode <83662999+SpwCode@users.noreply.github.com>
2021-05-04 23:23:38 +0500

    Update ExampleModule.java

--- src/main/java/meteordevelopment/addons/example/modules/ExampleModule.java
+++ src/main/java/meteordevelopment/addons/example/modules/ExampleModule.java
@@ -8,6 +8,17 @@ import minegame159.meteorclient.events.world.TickEvent;
 import minegame159.meteorclient.systems.modules.Categories;
 import minegame159.meteorclient.systems.modules.Modules;
 import minegame159.meteorclient.systems.modules.player.AutoEat;
+import minegame159.meteorclient.settings.BoolSetting;
+import minegame159.meteorclient.settings.IntSetting;
+import minegame159.meteorclient.settings.Setting;
+import minegame159.meteorclient.settings.SettingGroup;
+import minegame159.meteorclient.gui.GuiTheme;
+import minegame159.meteorclient.gui.widgets.WWidget;
+import minegame159.meteorclient.gui.widgets.containers.WTable;
+import minegame159.meteorclient.gui.widgets.input.WTextBox;
+import minegame159.meteorclient.gui.widgets.pressable.WMinus;
+import minegame159.meteorclient.gui.widgets.pressable.WPlus;
+import minegame159.meteorclient.utils.Utils;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
@@ -20,20 +31,106 @@ import java.util.Optional;
 
 
 public class ExampleModule extends Module {
-    private final List<String> messages = new ArrayList<>(); //add message list in settings menu
+     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    
+    private final Setting<Integer> delay = sgGeneral.add(new IntSetting.Builder()
+            .name("delay")
+            .description("The delay between specified messages in seconds.")
+            .defaultValue(10)
+            .min(0)
+            .sliderMax(30)
+            .build()
+    );
+    
+    private final Setting<Boolean> random = sgGeneral.add(new BoolSetting.Builder()
+            .name("random")
+            .description("Selects a random message from your spam message list.")
+            .defaultValue(false)
+            .build()
+    );
+    
+    private final List<String> messages = new ArrayList<>();
+    private int timer;
+    private int messageI;
     
     public ExampleModule() {
-        super(ExampleAddon.CATEGORY, "example", "This is an example module inside a custom category.");
+        super(ExampleAddon.CATEGORY, "autologin", "This is an example module inside a custom category.");
     }
     
+    @Override
+    public void onActivate() {
+        timer = delay.get() * 20;
+        messageI = 0;
+    }
+
     @EventHandler
     private void onTick(TickEvent.Post event) {
         if (mc.player == null) return;
         if (mc.world == null) return;
         if (messages.isEmpty()) return;
 
-        if (mc.player.getPos().getY() != 4) {
-             mc.player.sendChatMessage("4");
+        if (timer <= 0) {
+            int i;
+            if (random.get()) {
+                i = Utils.random(0, messages.size());
+            } else {
+                if (messageI >= messages.size()) messageI = 0;
+                i = messageI++;
+            }
+        if (mc.player.getPos().getY() == 4) {
+            timer = 0;
+            mc.player.sendChatMessage(messages.get(i));
+        }
+
+            timer = delay.get() * 20;
+        } else {
+            timer--;
         }
     }
+    
+    
+    @Override
+    public WWidget getWidget(GuiTheme theme) {
+        messages.removeIf(String::isEmpty);
+
+        WTable table = theme.table();
+        fillTable(theme, table);
+
+        return table;
+    }
+
+    private void fillTable(GuiTheme theme, WTable table) {
+        table.add(theme.horizontalSeparator("Messages")).expandX();
+        table.row();
+
+        // Messages
+        for (int i = 0; i < messages.size(); i++) {
+            int msgI = i;
+            String message = messages.get(i);
+
+            WTextBox textBox = table.add(theme.textBox(message)).minWidth(100).expandX().widget();
+            textBox.action = () -> messages.set(msgI, textBox.get());
+
+            WMinus delete = table.add(theme.minus()).widget();
+            delete.action = () -> {
+                messages.remove(msgI);
+
+                table.clear();
+                fillTable(theme, table);
+            };
+
+            table.row();
+        }
+
+        // New Message
+        WPlus add = table.add(theme.plus()).expandCellX().right().widget();
+        add.action = () -> {
+            messages.add("");
+
+            table.clear();
+            fillTable(theme, table);
+        };
+    }
+
+    
 }
